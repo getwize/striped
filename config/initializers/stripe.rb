0ed024c67bdab56f5020ee9ccb2d434dd294f3c7
muriithiKabogo <dmuriithi.k@gmail.com>
2017-05-18 18:15:54 +0000

    live

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,20 +2,20 @@
 
 if Rails.env.production?
 
-Rails.configuration.stripe = {
-  :publishable_key => ENV['stripe_publishable_key'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
-}
+	Rails.configuration.stripe = {
+	  :publishable_key => ENV['stripe_publishable_key_pro'],
+	  :secret_key      => ENV['STRIPE_SECRET_KEY_PRO']
+	}
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+	Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
  else
 
- 	Rails.configuration.stripe = {
-  :publishable_key => ENV['stripe_publishable_key'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
-}
+	 Rails.configuration.stripe = {
+	  :publishable_key => ENV['stripe_publishable_key'],
+	  :secret_key      => ENV['STRIPE_SECRET_KEY']
+	}
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+	Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
 end
